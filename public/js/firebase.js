de17feb7d8338f022057b0afcf87d1c8b4a9e45c
KashishSaini19 <79864759+KashishSaini19@users.noreply.github.com>
2022-05-27 01:00:56 +0530

    version 1.0

---
+++ public/js/firebase.js
@@ -0,0 +1,12 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyCMPu7abu9qojc9Bbbag-HGXPeKajHQV8g",
+    authDomain: "blogging-website-75e74.firebaseapp.com",
+    projectId: "blogging-website-75e74",
+    storageBucket: "blogging-website-75e74.appspot.com",
+    messagingSenderId: "623270820359",
+    appId: "1:623270820359:web:dfbfd29d263118245348a2"
+  };
+  
+   firebase.initializeApp(firebaseConfig);
+
+  let db = firebase.firestore();
