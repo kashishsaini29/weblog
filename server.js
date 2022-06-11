cd0a9ce0c112bb74caf1a651928c45de2a0d560a
KashishSaini19 <79864759+KashishSaini19@users.noreply.github.com>
2022-06-11 19:59:47 +0530

    version 1.1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(initial_path));
 app.use(fileupload());
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, "index.html"));
+    res.sendFile(path.join(initial_path, "home.html"));
 })
 
 app.get('/editor', (req, res) => {
@@ -24,22 +24,23 @@ app.post('/upload', (req, res) => {
     let imagename = date.getDate() + date.getTime() + file.name;
     // image upload path
     let path = 'public/uploads/' + imagename;
-  // create upload
-  file.mv(path, (err, result) => {
-    if(err){
-        throw err;
-    } else{
-        // our image upload path
-        res.json(`uploads/${imagename}`)
-    }
-})
+
+    // create upload
+    file.mv(path, (err, result) => {
+        if(err){
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
+        }
+    })
 })
 
-app.get("/:blog", (req,res) =>{
+app.get("/:blog", (req, res) => {
     res.sendFile(path.join(initial_path, "blog.html"));
 })
 
-app.use((req,res) =>{
+app.use((req, res) => {
     res.json("404");
 })
 
